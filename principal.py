6a15c191d2b580a029592daac78100948139797b
sophielvstzu <166159497+sophielvstzu@users.noreply.github.com>
2025-05-21 21:47:25 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,16 +1,16 @@
 import flet as ft
-from flet import icons 
+from flet import icons
 from administracion import administracion
 from estadisticas import estadisticas
-class UIcuy (ft.Container):
-    def __init__ (self, page: ft.Page):
-        super(). __init__ (expand = True)
-        self.page =page
+from granja import Granja
+
+class UIcuy(ft.Container):
+    def __init__(self, page: ft.Page):
+        super().__init__(expand=True)
+        self.page = page
         self.page.padding = 0
-        
-        
-        
-        ###colores a utilizar
+
+        # Colores
         self.color_blue = "#73b1fc"
         self.color_purple = "#bb7efd"
         self.bg_color = "#3f3965"
@@ -18,11 +18,11 @@ class UIcuy (ft.Container):
         self.color_navegation_bt = "#2a2949"
         self.color1_card = "#f46fd8"
         self.color2_card = "#64a4ed"
-        #####
+
         self.page.bgcolor = self.bg_color
-        #animacion
         self.animation_style = ft.animation.Animation(500, ft.AnimationCurve.EASE_IN_TO_LINEAR)
-        
+
+        # Contenedores de páginas
         self.container_1  = ft.Container(expand=True,
                           bgcolor= self.container_color,
                           offset= ft.transform.Offset(0,0),
@@ -159,177 +159,100 @@ class UIcuy (ft.Container):
                               ]
                           )       
                           )
+        self.container_2 = ft.Container(expand=True, bgcolor=self.container_color, content=ft.Container(padding=20, content=administracion()))
+        self.container_3 = ft.Container(expand=True, bgcolor=self.container_color, content=ft.Container(padding=20, content=estadisticas()))
+        self.container_4 = ft.Container(expand=True, bgcolor=self.container_color)
+        self.container_5 = ft.Container(expand=True, bgcolor=self.container_color, content=ft.Container(padding=20, content=Granja()))
 
-        self.container_2  = ft.Container(expand=True,
-                          bgcolor= self.container_color,
-                          offset= ft.transform.Offset(0,2),
-                          animate_offset=self.animation_style,
-                          content=ft.Container(
-                              padding=20,
-                              content=administracion()
-                          )            
-                          )
-        self.container_3 = ft.Container(expand=True,
-                          bgcolor= self.container_color,
-                          offset= ft.transform.Offset(0,3),
-                          animate_offset=self.animation_style,   
-                          content=ft.Container(
-                              padding = 20, 
-                              content= estadisticas() 
-                          )           
-                          )
-        self.container_4 = ft.Container(expand=True,
-                          bgcolor= self.container_color,
-                          offset= ft.transform.Offset(2,0),
-                          animate_offset=self.animation_style              
-                          )
-        
-        self.frame =ft.Container(
-            expand = True,
-            content = ft.Stack(
-                controls = (
-                self.container_1,
-                self.container_2, 
-                self.container_3,
-                self.container_4
-                )
-             )
-        ) 
-        self.option_1=ft.Container(padding=10,
-                                   bgcolor = self.color_purple,
-                                   border_radius= 15,
-                                   offset = ft.transform.Offset(0,0),
-                                   animate_offset=self.animation_style,
-                                   on_click=lambda e: self.change_page (e, 1),
-                                   height= 40, 
-                                   content = ft.Row(
-                                       alignment= ft.MainAxisAlignment.CENTER,
-                                       controls= [
-                                           ft.Icon( ft.icons.DIRECTIONS, color="white"),
-                                           ft.Text("Inicio", width = 120)
-                                       ]
-                                   )
-                             )
-        self.option_2=ft.Container(padding=10,
-                                   bgcolor = self.color_navegation_bt,
-                                   border_radius= 15,
-                                   offset = ft.transform.Offset(0,0),
-                                   animate_offset=self.animation_style,
-                                   on_click=lambda e: self.change_page (e, 2),
-                                   height= 40, 
-                                   content = ft.Row(
-                                       alignment= ft.MainAxisAlignment.CENTER,
-                                       controls= [
-                                           ft.Icon( ft.icons.MONETIZATION_ON_SHARP, color="white"),
-                                           ft.Text("Administración", width = 120)
-                                       ]
-                                   )
-                             )
-        self.option_3=ft.Container(padding=10,
-                                   bgcolor = self.color_navegation_bt,
-                                   border_radius= 15,
-                                   offset = ft.transform.Offset(0,0),
-                                   animate_offset=self.animation_style,
-                                   on_click=lambda e: self.change_page (e, 3),
-                                   height= 40, 
-                                   content = ft.Row(
-                                       alignment= ft.MainAxisAlignment.CENTER,
-                                       controls= [
-                                           ft.Icon( ft.icons.STACKED_LINE_CHART, color="white"),
-                                           ft.Text("Estadisticas", width = 120)
-                                       ]
-                                   )
-                             )
-        self.option_4=ft.Container(padding=10,
-                                   bgcolor = self.color_navegation_bt,
-                                   border_radius= 15,
-                                   offset = ft.transform.Offset(0,0),
-                                   animate_offset=self.animation_style,
-                                   on_click=lambda e: self.change_page (e, 4),
-                                   height= 40, 
-                                   content = ft.Row(
-                                       alignment= ft.MainAxisAlignment.CENTER,
-                                       controls= [
-                                           ft.Icon( ft.icons.SETTINGS_ACCESSIBILITY, color="white"),
-                                           ft.Text("Ayuda", width = 120)
-                                       ]
-                                   )
-                             )
+        self.frame = ft.Container(
+            expand=True,
+            content=ft.Stack(
+                controls=[
+                    self.container_1,
+                    self.container_2,
+                    self.container_3,
+                    self.container_4,
+                    self.container_5
+                ]
+            )
+        )
+
+        # Botones de navegación
+        self.option_1 = self.create_nav_option("Inicio", icons.DIRECTIONS, lambda e: self.change_page(e, 1), True)
+        self.option_2 = self.create_nav_option("Administración", icons.MONETIZATION_ON_SHARP, lambda e: self.change_page(e, 2))
+        self.option_3 = self.create_nav_option("Estadísticas", icons.STACKED_LINE_CHART, lambda e: self.change_page(e, 3))
+        self.option_4 = self.create_nav_option("Ayuda", icons.SETTINGS_ACCESSIBILITY, lambda e: self.change_page(e, 4))
+        self.option_5 = self.create_nav_option("Granja", icons.AGRICULTURE, lambda e: self.change_page(e, 5))
+
         self.navegation = ft.Container(
             padding=20,
-            bgcolor= self.container_color,
-            animate_size= self.animation_style,
-            content= ft.Column(
+            bgcolor=self.container_color,
+            animate_size=self.animation_style,
+            content=ft.Column(
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                 alignment=ft.MainAxisAlignment.CENTER,
                 controls=[
                     self.option_1,
                     self.option_2,
                     self.option_3,
-                    self.option_4 
+                    self.option_4,
+                    self.option_5
                 ]
-                
             )
         )
-        self.swich_control= {
+
+        self.swich_control = {
             1: self.container_1,
             2: self.container_2,
             3: self.container_3,
             4: self.container_4,
+            5: self.container_5
         }
+
         self.page.add(
             ft.Row(
-                expand = True,
-                spacing = 20,
-                controls = [
-                    self.navegation, 
-                    self.frame,
+                expand=True,
+                spacing=20,
+                controls=[
+                    self.navegation,
+                    self.frame
                 ]
             )
-            
-        ) 
-    def change_page(self,e,n):
-        for page in self.swich_control:
-            self.swich_control[page].offset.y = 2
-            self.swich_control[page].update() 
-            self.option_1.bgcolor = self.color_navegation_bt
-            self.option_2.bgcolor = self.color_navegation_bt
-            self.option_3.bgcolor = self.color_navegation_bt
-            self.option_4.bgcolor = self.color_navegation_bt
-        if n == 1:
-            self.option_1.offset.x = 0.15
-            self.option_2.offset.x = 0
-            self.option_3.offset.x = 0
-            self.option_4.offset.x = 0
-            self.option_1.bgcolor = self.color_purple
-            self.option_1.update()
-            
-        elif n == 2:
-            self.option_1.offset.x = 0.
-            self.option_2.offset.x = 0.15
-            self.option_3.offset.x = 0
-            self.option_4.offset.x = 0
-            self.option_2.bgcolor = self.color_purple
-            self.option_2.update()
-            
-        elif n == 3:
-            self.option_1.offset.x = 0
-            self.option_2.offset.x = 0
-            self.option_3.offset.x = 0.15
-            self.option_4.offset.x = 0
-            self.option_3.bgcolor = self.color_purple
-            self.option_3.update()
-            
-        elif n == 4:
-            self.option_1.offset.x = 0
-            self.option_2.offset.x = 0
-            self.option_3.offset.x = 0
-            self.option_4.offset.x = 0.14
-            self.option_4.bgcolor = self.color_purple
-            self.option_4.update()
-        self.swich_control[n].offset.y = 0
-        self.swich_control[n].update() 
-        self.page.update()
-            
+        )
 
+    def create_nav_option(self, label, icon, on_click, selected=False):
+        return ft.Container(
+            padding=10,
+            bgcolor=self.color_purple if selected else self.color_navegation_bt,
+            border_radius=15,
+            offset=ft.transform.Offset(0.15 if selected else 0, 0),
+            animate_offset=self.animation_style,
+            on_click=on_click,
+            height=40,
+            content=ft.Row(
+                alignment=ft.MainAxisAlignment.CENTER,
+                controls=[
+                    ft.Icon(icon, color="white"),
+                    ft.Text(label, width=120, color="white")
+                ]
+            )
+        )
+
+    def change_page(self, e, n):
+        for i in self.swich_control:
+            self.swich_control[i].offset = ft.transform.Offset(0, 2)
+            self.swich_control[i].update()
+
+        for opt in [self.option_1, self.option_2, self.option_3, self.option_4, self.option_5]:
+            opt.bgcolor = self.color_navegation_bt
+            opt.offset = ft.transform.Offset(0, 0)
+            opt.update()
+
+        getattr(self, f"option_{n}").bgcolor = self.color_purple
+        getattr(self, f"option_{n}").offset = ft.transform.Offset(0.15, 0)
+        getattr(self, f"option_{n}").update()
+
+        self.swich_control[n].offset = ft.transform.Offset(0, 0)
+        self.swich_control[n].update()
+        self.page.update()
 
